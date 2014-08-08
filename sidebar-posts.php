530641734124dc1908800cda3365ac897bb34c84
Gambya <robertolopesramos@gmail.com>
2014-08-07 22:16:08 -0300

    adicionado mais duas sidebar ao template.

---
+++ sidebar-posts.php
@@ -0,0 +1,5 @@
+<div class="posts-newsletter">
+    <?php if(!function_exists('dynamic_sidebar') || !dynamic_sidebar('posts')): ?>
+
+    <?php endif; ?>
+</div>
---
+++ sidebar-search.php
@@ -0,0 +1,5 @@
+<div class="search">
+    <?php if(!function_exists('dynamic_sidebar') || !dynamic_sidebar('search')): ?>
+
+    <?php endif; ?>
+</div>
